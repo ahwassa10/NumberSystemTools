ab2562951b7ad27f9fb1b6e3b31452bb19705571
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2020-11-01 23:01:38 -0500

    Began using styles

--- gui.py
+++ gui.py
@@ -5,10 +5,20 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+
+style = ttk.Style()
+style.configure("T.TFrame", borderwidth = 10, relief = "sunken")
+
 content = ttk.Frame(root).grid()
+numbers = ttk.Frame(content).grid(row = 1, column = 0)
+#options = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100).grid(row = 1, column = 1)
 
-decimalEntry = ttk.Entry(content, textvariable="decimal").grid(row = 0, column = 0)
+decimalFrame = ttk.Frame(numbers, style="T.TFrame", padding = (3, 3, 12, 12)).grid(row = 0, column = 0)
+decimalLabel = ttk.Label(decimalFrame, text = "Decimal").grid(row = 0, column = 0)
+decimalEntry = ttk.Entry(decimalFrame, textvariable="decimal").grid(row = 1, column = 0)
 
-hexadecimalEntry = ttk.Entry(content, textvariable="hexadecimal").grid(row = 1, column = 0)
+#hexadecimalEntry = ttk.Entry(numbers, textvariable="hexadecimal").grid(row = 1, column = 0)
 
-binaryEntry = ttk.Entry(content, textvariable="Binary").grid(row = 2, column = 0)
+#binaryEntry = ttk.Entry(numbers, textvariable="Binary").grid(row = 2, column = 0)
+
+root.mainloop()
